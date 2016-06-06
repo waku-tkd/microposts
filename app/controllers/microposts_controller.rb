334d1be6a25525f5b3627fc8d6f8f30e8ade469f
Yu Takeda <yu-takeda@dac.co.jp>
2016-06-06 08:14:47 +0000

    201606061714

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -7,10 +7,11 @@ class MicropostsController < ApplicationController
       flash[:success] = "Micropost created!"
       redirect_to root_url
     else
+      @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc) # この行を追加
       render 'static_pages/home'
     end
-  end
-  
+  end  
+
   def destroy
     @micropost = current_user.microposts.find_by(id: params[:id])
     return redirect_to root_url if @micropost.nil?
