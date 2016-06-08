0e91971ca4276162bd9f471ce5a5caa52e168efb
Yu Takeda <yu-takeda@dac.co.jp>
2016-06-08 04:43:01 +0000

    add for restrict

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
     before_action :logged_in_user, only: [:edit, :update]
+    before_action :correct_user,   only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -48,4 +49,8 @@ class UsersController < ApplicationController
        end
   end  
 
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless @user == current_user
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  validates :location, length: { maximum: 50 }
   has_secure_password
   has_many :microposts
   has_many :following_relationships, class_name:  "Relationship",
